621405e712379396e4bfd66ceb1650d05dc53f14
chiudaniels <daniel.sean.chiu@gmail.com>
2017-02-14 12:37:24 -0500

    done

--- canvas.js
+++ canvas.js
@@ -1,10 +1,23 @@
 var canv= document.getElementById("area");
 var ctx= canv.getContext("2d"); 
+ctx.beginPath();
 function drawRect(e){
-    ctx.fillRect(e.clientX-65,e.clientY-10,20,20);
+    ctx.fillRect(e.offsetX,e.offsetY,20,20);
 }
-canv.addEventListener("click",drawRect)
+
+//canv.addEventListener("click",drawRect)
 
 function clicked(){
-	ctx.clearRect(0,0,500,500);
+    ctx.clearRect(0,0,500,500);
+    ctx.beginPath();
+}
+
+canv.addEventListener("click",drawCircle);
+
+function drawCircle(e){
+    ctx.lineTo(e.offsetX,e.offsetY);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(e.offsetX,e.offsetY,20,0,2*Math.PI);
+    ctx.fill();
 }
